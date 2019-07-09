7bd63a675e55096c979116663d34cb900323f847
zhanhui <hui.zhan06@hand-china.com>
2019-07-09 16:37:23 +0800

    vue项目 服务器部署 nginx配置代理解决跨域

--- cloud-store-admin/src/router/index.js
+++ cloud-store-admin/src/router/index.js
@@ -5,6 +5,8 @@ import Router from 'vue-router'
 import Layout from '../views/layout/Layout'
 
 const _import = require('./_import_' + process.env.NODE_ENV);
+console.log(process.env.NODE_ENV,'NODE_ENV');
+console.log(process.env.BASE_URL,'BASE_URL');
 Vue.use(Router);
 export const constantRouterMap = [
   {path: '/login', component: _import('login/index'), hidden: true},
